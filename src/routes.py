8530ecb3891a8848f22650662c056c0d55e0668b
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-15 23:24:40 +0700

    Support template inheritance

--- src/routes.py
+++ src/routes.py
@@ -3,8 +3,14 @@ from src import app
 
 # Debugging purposes only
 user = {'username': 'Unknown1508', 'displayname': 'Unknown'}
+ratinglist = [
+    {
+    'user': user,
+    'rating': 2278
+    }
+]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', user = user)
+    return render_template('index.html', ratinglist = ratinglist)
