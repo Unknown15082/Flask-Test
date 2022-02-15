1c1c822e1e8c1bb59a13948f1f4e072331095b2a
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-15 22:48:35 +0700

    Now support templates

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from src import app
+
+# Debugging purposes only
+user = {'username': 'Unknown1508', 'displayname': 'Unknown'}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello"
+    return render_template('index.html', user = user)
