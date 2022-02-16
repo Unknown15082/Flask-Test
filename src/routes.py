93524d5c7255180b9d07406f473b1c5beca908fc
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-16 12:55:15 +0700

    Edit purpose of the website :)

--- src/routes.py
+++ src/routes.py
@@ -3,14 +3,18 @@ from src import app
 
 # Debugging purposes only
 user = {'username': 'Unknown1508', 'displayname': 'Unknown'}
-ratinglist = [
+blogs = [
     {
-    'user': user,
-    'rating': 2278
+        'author_id': 1,
+        'content': 'Some random content'
+    },
+    {
+        'author_id': 2,
+        'content': 'Extra random stuff'
     }
 ]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', ratinglist = ratinglist)
+    return render_template('index.html', blog_list = blogs)
