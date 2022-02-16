b4a4b4c1d96965007b070a0678ff78b8821aa912
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-16 20:56:52 +0700

    Added posts to SQL schema

--- src/models.py
+++ src/models.py
@@ -6,6 +6,17 @@ class User(db.Model):
     displayname = db.Column(db.String(64))
     email = db.Column(db.String(128), index=True, unique=True)
     pwd = db.Column(db.String(128))
+
+    posts = db.relationship('Post', backref='author', lazy = 'dynamic')
     
     def __repr__(self):
         return f'User {self.displayname}'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128))
+    body = db.Column(db.String(2048))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'[{self.title}]: [{self.body}]'
