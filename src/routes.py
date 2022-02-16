39c9c8c0ce44930af2d5f971a034b65a94c88952
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-16 17:31:59 +0700

    Uses url_for for links

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from src import app
 from src.forms import LoginForm
 
@@ -29,5 +29,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'LOGIN requested for: User = {form.username.data}')
-        redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title = 'Login', form = form)
